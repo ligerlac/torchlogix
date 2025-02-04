a6b262299f11c6fcda8a6a5ffed9d3ce97935bb9
liv <liv.helen.vage@cern.ch>
2025-02-04 09:59:00 +0100

    :bug: WIP: Add implementation and connection back as arguments for CNN

--- difflogic/difflogic.py
+++ difflogic/difflogic.py
@@ -240,6 +240,8 @@ class LogicCNNLayer(torch.nn.Module):
             num_kernels: int = 16,
             tree_depth: int = None,
             receptive_field_size: int = None,
+            implementation: str = None,
+            connections: str = 'random',
             stride: int = None,
             padding: int = None
     ):
