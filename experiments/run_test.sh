2cadab57228644482e0ebb0bad7e88005062b7d3
gerlach <tgerlac1@uni-bonn.de>
2025-09-28 19:53:10 +0200

    Ignore test

--- experiments/run_test.sh
+++ experiments/run_test.sh
@@ -1,1 +1,7 @@
-python3 train.py --dataset mnist20x20 --implementation python --batch-size 32 --eval-freq 200 --num-iterations 1000 --seed 1 --parametrization walsh -o results/walsh-random/1
+# python3 train.py --dataset mnist20x20 --implementation python --batch-size 32 --eval-freq 200 --num-iterations 1000 --seed 1 --parametrization walsh --walsh-sampling sigmoid -o results/walsh-sigmoid/1
+
+# python3 train.py --dataset mnist20x20 --implementation python --batch-size 32 --eval-freq 200 --num-iterations 1000 --seed 1 --parametrization walsh --walsh-sampling softmax -o results/walsh-softmax/1
+
+python3 train.py --dataset mnist20x20 --implementation python --batch-size 32 --eval-freq 500 --num-iterations 1000 --seed 1 --parametrization walsh --walsh-sampling gumbel_hard -o results/walsh-gumbel_hard/1
+
+# python3 train.py --dataset mnist20x20 --implementation python --batch-size 32 --eval-freq 500 --num-iterations 1000 --seed 1 --parametrization walsh --walsh-sampling gumbel_soft -o results/walsh-gumbel_soft/1
