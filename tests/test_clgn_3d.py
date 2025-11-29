78e94c904677f19bc48f495937c5d77b7a486a80
ThoreGerlach <thore@villabunt.de>
2025-11-29 17:11:53 +0100

    Removed lut_rank=6 test for reducing memory overhead

--- tests/test_clgn_3d.py
+++ tests/test_clgn_3d.py
@@ -292,26 +292,8 @@ def test_binary_model():
 
 
 def test_lut_rank_walsh():
-    """Test scaling up to multiple inputs, that is n=4 and n=6."""
+    """Test scaling up to multiple inputs, that is n=4."""
     lut_rank = 4
-    layer = layer = LogicConv3dWalsh(
-        in_dim=(3, 4, 3),
-        device="cpu",
-        channels=1,
-        num_kernels=1,
-        tree_depth=0,
-        receptive_field_size=3,
-        connections="random-unique",
-        stride=1,
-        padding=0,
-        lut_rank=lut_rank,
-    )
-    luts, ids = layer.get_lut_ids()
-    for luts_level in luts:
-        for luts_ in luts_level:
-            assert luts_.shape[-1] == 1 << lut_rank
-
-    lut_rank = 6
     layer = layer = LogicConv3dWalsh(
         in_dim=(3, 4, 3),
         device="cpu",
--- tests/test_lgn.py
+++ tests/test_lgn.py
@@ -96,18 +96,9 @@ def test_xor_model_walsh():
 
 
 def test_lut_rank_walsh():
-    """Test scaling up to multiple inputs, that is n=4 and n=6."""
+    """Test scaling up to multiple inputs, that is n=4."""
     x = 1 - 2 * torch.rand((1, 12))
     lut_rank = 4
-    out_dim = x.shape[1] // lut_rank
-    layer = LogicDenseWalsh(in_dim=x.shape[1], out_dim=out_dim, lut_rank=lut_rank, **llkw)
-    luts, ids = layer.get_lut_ids()
-    assert luts.shape == (out_dim, 1 << lut_rank)
-    model = torch.nn.Sequential(layer)
-    y = model(x)
-    assert y.shape == (x.shape[0], out_dim)
-
-    lut_rank = 6
     out_dim = x.shape[1] // lut_rank
     layer = LogicDenseWalsh(in_dim=x.shape[1], out_dim=out_dim, lut_rank=lut_rank, **llkw)
     luts, ids = layer.get_lut_ids()
